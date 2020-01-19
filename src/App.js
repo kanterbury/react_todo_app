631e5b2f7ebc1ad84d629ec9758d3c8cf48a6bdc
kanterbury <ksuzuki.db@gmail.com>
2020-01-19 15:46:57 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+import Memo from './memo/Memo';
+import AddForm from './memo/AddForm';
+import FindForm from './memo/FindForm';
+
+// Appコンポーネント
+class App extends Component {
+  td = {
+    width:"250px"
+  }
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Memo</h1>
+        <AddForm />
+        <hr />
+        <table><tbody><tr>
+          <td style={this.td}><FindForm /></td>
+        </tr></tbody></table>
+        <Memo />
+      </div>
+    );
+  }
+}
+
+
+export default connect()(App);
---
+++ src/memo/Item.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteMemo } from './Store';
+
+
+class Item extends Component {
+
+  th = {
+    fontSize:"14pt",
+    backgroundColor:"blue",
+    color:"white",
+    padding:"5px 10px",
+    width:"50px"
+  }
+  td = {
+    fontSize:"14pt",
+    backgroundColor:"white",
+    color:"darkblue",
+    padding:"5px 10px",
+    border:"1px solid lightblue",
+    minWidth:"300px"
+  }
+  date = {
+    fontSize:"14pt",
+    backgroundColor:"white",
+    color:"darkblue",
+    padding:"5px 10px",
+    border:"1px solid lightblue",
+    width:"80px"
+  }
+  del = {
+    fontSize:"14pt",
+    backgroundColor:"white",
+    color:"darkblue",
+    padding:"5px 10px",
+    border:"1px solid lightblue",
+    width:"80px"
+  }
+  btn = {
+    fontSize:"12pt",
+    color:"#006",
+    padding:"2px 10px"
+  }
+
+  constructor(props){
+    super(props);
+
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e){
+    e.preventDefault();
+    console.log(this.props.index);
+    let action = deleteMemo(this.props.index);
+    this.props.dispatch(action);
+  }
+
+  
+
+  render(){
+    let d = this.props.value.created;
+    let f = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+    return (
+    <tr><th style={this.th}>No, {this.props.index}</th>
+      <td style={this.td}>{this.props.value.message}</td>
+      <td style={this.date}>{f}</td>
+      <td style={this.del}><button style={this.btn} onClick={this.doAction}>Del</button></td>
+    </tr>
+    );
+  }
+}
+export default connect()(Item);
